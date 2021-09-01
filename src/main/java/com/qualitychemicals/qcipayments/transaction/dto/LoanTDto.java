9a608d186d8949736dd2b5548115a26c14516fe0
lodkazibwe <lodkazibwe@gmail.com>
2021-09-01 07:48:40 +0300

    loan ref

--- src/main/java/com/qualitychemicals/qcipayments/transaction/dto/LoanTDto.java
+++ src/main/java/com/qualitychemicals/qcipayments/transaction/dto/LoanTDto.java
@@ -11,5 +11,5 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class LoanTDto extends TransactionDto {
     private int loanId;
-    private String loanRef;
+    //private String loanRef;
 }
